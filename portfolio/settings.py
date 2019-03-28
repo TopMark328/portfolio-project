a85117c4c6fd7ca700a59815d4c8f62568a7e3b4
TopMark328 <topmarksolutions18@gmail.com>
2019-03-27 20:41:05 -0400

    requirements

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'djangodbman',
-        'PASSWORD': 'django1234',
+        'USER': 'postgres',
+        'PASSWORD': 'tariq1',
         'HOST':'localhost',
         'PORT':'5432',
     }
